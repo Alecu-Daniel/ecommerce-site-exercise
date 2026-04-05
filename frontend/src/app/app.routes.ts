489a4b438294119bc282e675ac00880706fde254
Dany <Dany@DESKTOP-Q79QAPR>
2026-04-05 13:30:38 +0300

    Added a navbar with router links

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ProductListComponent } from './components/product-list/product-list';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 export const routes: Routes = [
   { path: '', component: ProductListComponent },
--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -6,8 +6,8 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-product-list',
   imports: [CommonModule],
-  templateUrl: './product-list.html',
-  styleUrl: './product-list.css',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent implements OnInit {
   products = signal<Product[]>([]);
